7b063602473350f0889806b0d8c271a123e63748
Mcswiggi <mrchitrason@gmail.com>
2021-08-29 18:59:12 -0400

    add finch detail page

--- main_app/models.py
+++ main_app/models.py
@@ -7,3 +7,7 @@ class Finch(models.Model):
   breed = models.CharField(max_length=100)
   description = models.TextField(max_length=250)
   age = models.IntegerField() 
+
+#this is what lets the name show in the admin portal vs object numbers
+  def __str__(self):
+    return self.name  
--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Finch
 
 # Create your views here.
 def home(request):
@@ -9,4 +9,9 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', { 'finches': finches }) #setting finches to the array above
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', { 'finch': finch })
