0050700b6219539660d88d79c82efb9c4f040c25
Mcswiggi <mrchitrason@gmail.com>
2021-08-30 22:14:11 -0400

    add feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+MEALS = (
+  ('B', 'Breakfast'),
+  ('L', 'Lunch'),
+  ('D', 'Dinner')
+)
 
 class Finch(models.Model):
   name = models.CharField(max_length=100)
@@ -10,4 +15,21 @@ class Finch(models.Model):
 
 #this is what lets the name show in the admin portal vs object numbers
   def __str__(self):
-    return self.name  
+    return self.name
+  
+  def get_absolute_url(self):
+    return reverse('finches_detail', kwargs={'finches_id': self.id})  
+
+#adding second model-feeding
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+    max_length=1,
+    choices=MEALS,
+    default=MEALS[0][0]
+  )
+
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f"{self.get_meal_display()} on {self.date}"
