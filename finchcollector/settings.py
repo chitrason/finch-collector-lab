abeaa6f1dc7396ea834dbd71eb6bce477311ef63
Mcswiggi <mrchitrason@gmail.com>
2021-09-01 15:39:25 -0400

    adjust about page

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-# SECRET_KEY = 'django-insecure-8v7a@(dic0n*v9ex8a=gu&gdbd6wz&*8e$o_ucmu!!&27$wkr&'
+SECRET_KEY = 'django-insecure-8v7a@(dic0n*v9ex8a=gu&gdbd6wz&*8e$o_ucmu!!&27$wkr&'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
